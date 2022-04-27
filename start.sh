aa7a96805e222d5f5e006788982fda2adac2bae6
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-04-28 02:05:54 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Asifasif6625/LUCIFER1.git /LUCIFER1
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LUCIFER1
+fi
+cd /LUCIFER1
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
