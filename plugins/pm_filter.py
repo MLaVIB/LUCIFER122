3992f2717bf3949d355435eb97e242c8e4e07f1c
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-09-22 13:47:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -645,7 +645,8 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        await message.reply_photo('https://telegra.ph/file/a28a7ea96700495576614.jpg') 
+        await message.reply_photo('https://telegra.ph/file/592b83106a602598aedaf.jpg')
+        await message.delete()
         cap = f"⚠️കോപ്പിറൈറ്റ് ഉള്ളത് കൊണ്ട് നിങ്ങൾ ചോദിച്ച {search} മൂവി 5 മിനിറ്റ് കൊണ്ട് ഇവിടെ നിന്ന് ഡിലീറ്റ് ആകും!!!"
         btn.append(
             [InlineKeyboardButton('🔰𝙅𝙤𝙞𝙣 𝙘𝙝𝙖𝙣𝙣𝙚𝙡🔰', url='t.me/Malayalamvibe')]
